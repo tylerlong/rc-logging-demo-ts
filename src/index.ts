af67402023fd8212293ead2a6da6583cbeba2901
Tyler Long <tyler4long@gmail.com>
2022-03-16 17:49:36 -0700

    First working version

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello world');
+import RingCentral from '@rc-ex/core';
+
+const rc = new RingCentral({
+  server: 'https://platform.ringcentral.com',
+  clientId: 'fake',
+  clientSecret: 'fake',
+});
+
+RingCentral.config.logger = console;
+
+const main = async () => {
+  await rc.authorize({
+    username: 'fake',
+    extension: '',
+    password: 'fake',
+  });
+};
+
+main();
